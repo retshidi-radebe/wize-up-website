e5f28fbcadcc5f02c69b7a30c1077f46c3706af4
retshidi-radebe <retshidi10@gmail.com>
2025-06-22 18:23:24 +0200

    team updates

--- js/main.js
+++ js/main.js
@@ -82,6 +82,22 @@
         ]
     });
 
+    
+    $('.team-carousel').owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      navText: [
+        '<i class="fa fa-chevron-left"></i>',
+        '<i class="fa fa-chevron-right"></i>'
+      ],
+      responsive: {
+        0: { items: 1 },
+        600: { items: 2 },
+        1000: { items: 3 }
+      }
+    });
+
 
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
